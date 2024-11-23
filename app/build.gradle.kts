fcb7dafdd0da55d86aa700e73db81b713d0d16cc
aggalan <aggalan@itba.edu.ar>
2024-11-22 21:51:09 -0300

    qr

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,8 @@ dependencies {
     implementation(libs.androidx.camera.view)
     implementation(libs.mlkit.barcode.scanning)
     implementation (libs.kotlinx.serialization.json.v132)
-    implementation(libs.accompanist.permissions) // Check for missing semicolons or extra commas.
+    implementation(libs.accompanist.permissions)
+    implementation(libs.zxing)// Check for missing semicolons or extra commas.
 
 
 }
